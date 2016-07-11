2c3273ab20fbddd2f27d24c4540d5057f6744dd5
MaxR <q121212@gmail.com>
2016-07-11 13:01:00 +0300

    Image file format v.0.0.2

--- Image file format/iff.py
+++ Image file format/iff.py
@@ -63,39 +63,40 @@
 # [0,0,0,0,0,0,0,0,0,0],
 # [0,0,0,0,0,0,0,0,0,0]]
 
+# Lybraries import part
+from tkinter import *
+
+# a function do transponse from single-order list to double-order list
+def transponse(data, width):
+  count = 0
+  new_data_line = []
+  new_data = []
+  for i in data:
+    if count < width:
+      new_data_line.append(int(i))
+      count+=1
+    else:
+      new_data.append(new_data_line)
+      new_data_line = []
+      new_data_line.append(int(i))
+      count=1
+    
+  new_data.append(new_data_line)
+  return new_data
+
+      
 def openimagefile(filename, width):
   with open(filename, 'r') as f:
     data = f.read()
 
 
   data=data.split(',')
-  count = 0
-  new_data_line = []
-  new_data = []
-  for i in data:
-    if count < width:
-      new_data_line.append(i)
-      count+=1
-    else:
-      new_data.append(new_data_line)
-      new_data_line = []
-      new_data_line.append(i)
-      count=1
-
-      
-  new_data.append(new_data_line)
+  new_data = transponse(data, width)
   print('A file: ' + filename + ' was opened.')
-  print('Image height: ' + str(len(new_data)))
+  print('Image width:  {0}, height: {1}'.format(width, str(len(new_data))))
   print('Image size: ' + str(len(data)))
   return new_data
-  
-  
-  # print(len(data))
-  # with open('image3.txt', 'a') as f:
-    # for i in range(99999):
-      # for j in range(len(new_data)):
-        # for k in new_data[j]:
-          # f.write(str(k)+',')
+
 
 def generate_empty_image(width, height):
   image = []
@@ -104,21 +105,9 @@ def generate_empty_image(width, height):
       image.append(0)
   
   data = image
-  count = 0
-  new_data_line = []
-  new_data = []
-  for i in data:
-    if count < width:
-      new_data_line.append(i)
-      count+=1
-    else:
-      new_data.append(new_data_line)
-      new_data_line = []
-      new_data_line.append(i)
-      count=1
-
-  new_data.append(new_data_line)
-  print(len(image))
+  new_data = transponse(data, width)
+  print('Image with demensions: {0} x {1} (width x height) was generated'.format(width, height))
+  print('Image size: ' + str(len(image)))
   return new_data
   
 def save_image(image, filename):
@@ -130,12 +119,30 @@ def save_image(image, filename):
         else:
           f.write(str(image[i][j])+',')
   print('A file was written as: ' + filename)
+
+def image_view(image):
+  pass
   
   
+def draw_image_file(filename, image_width, canvas_width = 500, canvas_height = 500):
+  master = Tk()
+  w = Canvas(master, 
+             width=canvas_width, 
+             height=canvas_height)
+  w.pack()
+  image = openimagefile(filename, image_width)
+  for i in range(len(image)):
+    for j in range(len(image[i])):
+      if image[i][j] == 1:
+        w.create_oval(j,i,j,i)
+  mainloop()    
+
+ 
 def main():
   openimagefile('image.txt', 10)
   save_image(generate_empty_image(50, 20), 'image2.txt')
   save_image(openimagefile('image2.txt', 50), 'image3.txt')
+  draw_image_file('image4.txt', 500, 650, 650)
 
 if __name__ == '__main__':
   main()
