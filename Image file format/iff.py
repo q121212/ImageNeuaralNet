6f4c264ad62f7735c8c875176e769a0e6e8aa4e4
MaxR <q121212@gmail.com>
2016-07-13 21:33:12 +0300

    Rewrited methods: from paint_image & extract_image_from_painted_image was created paint_image_new. to replace the paint_image_and_save_to_file method, the method paint_image_new_and_save_image_with_metadata is written. Added methods: transponse_two_to_one_ordered_list(image), draw_image_file_with_metadata(filename)

--- Image file format/iff.py
+++ Image file format/iff.py
@@ -263,12 +263,122 @@ def extract_image_from_image_with_metadata(filename):
 
 def extract_image_width_from_image_with_metadata(filename):
   '''Method for extracting image_width from image file with metadata.'''
+  
   with open(filename, 'r') as f:
     data = f.read()
     
   return int(data.split('|')[1])
 
+
+def transponse_two_to_one_ordered_list(image):
+  '''Method for transponse image (two ordered list) to one orderedlist.'''
+
+  oneorderedlist = []
+  for i in range(len(image)):
+    for j in range(len(image[i])):
+      oneorderedlist.append(image[i][j]) 
+      
+  return oneorderedlist
+  
+  
+def paint_image_new(canvas_width = 500, canvas_height = 500):
+  '''New, reprocessed method for painting image (w\o 'positions.txt' file). The dimensions of canvas (width and height) must be specified.'''
+
+  # with open('positions.txt', 'w') as f:
+    # f.close()
+  positions = []
+  def paint_pixel_for_image( event ):
+    # with open('positions.txt', 'a') as f:
+    python_green = "#476042"
+    x, y = ( event.x ), ( event.y )
+    w.create_oval( x, y, x, y, fill = python_green )
+    result = [x, y]
+    print(result)
+    positions.append([result[0], result[1]])
+    return result
+  master = Tk()
+  master.title( "Painting canvas" )
+  w = Canvas(master, 
+             width=canvas_width, 
+             height=canvas_height)
+  w.pack(expand = YES, fill = BOTH)
+  arr = []
+  w.bind( "<B1-Motion>", paint_pixel_for_image )
+
+  message = Label( master, text = "Press and Drag the mouse to draw" )
+  message.pack( side = BOTTOM )
+      
+  mainloop()
+# this place comes into operation after the closure of the drawing window!!
+
+  coords_data = positions
+  new_coords_data = []
+  min_w_value = 10**12
+  max_w_value = 0
+  min_h_value = 10**12
+  max_h_value = 0
+  counter = 1
+
+  for i in range(len(coords_data)):
+    for j in range(len(coords_data[i])):
+      new_coords_data.append(int(coords_data[i][j]))
+      if counter%2 == 0:
+        if new_coords_data[-1] > max_h_value:
+          max_h_value = new_coords_data[-1]
+        if new_coords_data[-1] < min_h_value:
+          min_h_value = new_coords_data[-1]
+      else:
+        if new_coords_data[-1] > max_w_value:
+          max_w_value = new_coords_data[-1]
+        if new_coords_data[-1] < min_w_value:
+          min_w_value = new_coords_data[-1]     
+      
+      counter+=1
     
+  image = generate_empty_image(max_w_value,max_h_value)
+  
+  x_array = []
+  y_array = []
+  for i in range(len(new_coords_data)):
+    if i%2 == 0:
+      x_array.append(new_coords_data[i])
+    else:
+      y_array.append(new_coords_data[i])
+  
+  print(x_array, y_array)
+  for i in range(len(x_array)):
+    image[y_array[i]-1][x_array[i]-1] = 1
+     
+  return image
+
+
+def paint_image_new_and_save_image_with_metadata(filename, canvas_width = 500, canvas_height = 500):
+  '''Method for painting image and saving it to image with metadata.'''
+  
+  image = paint_image_new(canvas_width, canvas_height)
+  save_image_with_metadata(image, max_image_w_value(image), filename)
+
+
+def draw_image_file_with_metadata(filename):
+  '''Method for showing/displaying image with metadata.'''
+
+  image = extract_image_from_image_with_metadata(filename)
+  canvas_width = max_image_w_value(image)
+  canvas_height = max_image_h_value(image)
+  
+  master = Tk()
+  # value +50 is a random number for creating a frame (additional canvas space) for a more presentable view of image. In the future this values change to min_w_value & min_h_value (need calculate them (find where calculations are made and use it))
+  w = Canvas(master, 
+             width=canvas_width+50, 
+             height=canvas_height+50)
+  w.pack()
+  for i in range(len(image)):
+    for j in range(len(image[i])):
+      if image[i][j] == 1:
+        w.create_oval(j,i,j,i)
+  mainloop()  
+  
+  
 def main():
   # openimagefile('image.txt', 10)
   # save_image(generate_empty_image(50, 20), 'image2.txt')
@@ -285,7 +395,11 @@ def main():
   # save_image(extract_image_from_image_with_metadata('imagewithmeta.txt'), 'image8.txt')
   # draw_image_file('image8.txt', extract_image_width_from_image_with_metadata('imagewithmeta.txt'), 500, 600)
   # draw_image(extract_image_from_image_with_metadata('imagewithmeta.txt'), extract_image_width_from_image_with_metadata('imagewithmeta.txt'), 500, 600)
-
+  # paint_image_new(500, 500)
+  paint_image_new_and_save_image_with_metadata('image9.txt', 500, 600)
+  draw_image_file_with_metadata('image9.txt')
+  
+  
   pass
 
 if __name__ == '__main__':
