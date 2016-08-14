a5f5c02d4bf0613c629c7f73cc9450d6188273a0
MaxR <q121212@gmail.com>
2016-08-14 23:02:31 +0300

    Was started neurons.py

---
+++ neurons.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys, os
+lib_path = os.path.abspath(os.path.join('.', 'ImageFileFormat/'))
+sys.path.insert(0, lib_path)
+
+lib_path = os.path.abspath(os.path.join('.', 'Models/'))
+sys.path.insert(0, lib_path)
+
+lib_path = os.path.abspath(os.path.join('.', 'Extract/'))
+sys.path.insert(0, lib_path)
+
+lib_path = os.path.abspath(os.path.join('.', 'Transform/'))
+sys.path.insert(0, lib_path)
+
+import iff
+import models
+import extract
+import transform
+
+def neuron1(image):
+  return
+
+if __name__ == '__main__':
+  #  
+  print(1) 
+
+  pass
